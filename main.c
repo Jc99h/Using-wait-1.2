d7e4a4d02fa73e6a32c1f8e52ee911d48c679113
CamiloHurtado99 <95389919+Jc99h@users.noreply.github.com>
2021-12-11 01:53:03 +0000

    Finalizado, el padre captura el valor de salida del hijo

--- main.c
+++ main.c
@@ -6,5 +6,24 @@
 
 int main(int argc, char *argv[])
 {
-  
+  int aleatorio=maxrand(100);
+  int rc=fork();
+  int status;
+  int rc_wait=wait(&status);
+
+  if(rc==0)
+  {
+    printf("Soy el hijo (pid:%d)\n", getpid());
+    printf("Y el hijo retorna un numero aleatorio: %i\n", aleatorio);
+    return aleatorio;
+  }
+  else
+  {
+    printf("Soy el padre de %d (pid:%d)\n", rc, getpid());
+    if(WIFEXITED(status)!=0) //es distinto de cero si el hijo termino normalmente
+    {
+      printf("Soy el padre y obtengo el valor de salida el proceso hijo: %i\n", WEXITSTATUS(status));
+    }
+    return WEXITSTATUS(status);
+  }
 }
