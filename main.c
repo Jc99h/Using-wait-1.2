6adba74143513f4255b23b5b58f55e8b321bbe65
CamiloHurtado99 <95389919+Jc99h@users.noreply.github.com>
2021-12-11 00:52:25 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include "maxrand.h"
+
+int main(int argc, char *argv[])
+{
+  
+}
---
+++ maxrand.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+int maxrand(int max)
+{
+  srand(time(0));
+  return rand()%max;
+}
